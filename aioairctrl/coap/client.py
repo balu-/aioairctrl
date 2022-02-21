fd94ac69a478b97dc06a11be556e8d7d7fb0aaf2
balu <balu@mobile>
2022-02-21 20:03:35 +0100

    add callback to observ method

--- aioairctrl/coap/client.py
+++ aioairctrl/coap/client.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+from collections.abc import Callable
 
 from aioairctrl.coap import aiocoap_monkeypatch  # noqa: F401
 from aiocoap import (
@@ -71,7 +72,8 @@ class Client:
         state_reported = json.loads(payload)
         return state_reported["state"]["reported"]
 
-    async def observe_status(self):
+    async def observe_status(self, on_valuechange_callback: Callable[[dict], None] ):
+        """ Observe status call on_valuechange_callback when data changes"""
         def decrypt_status(response):
             payload_encrypted = response.payload.decode()
             payload = self._encryption_context.decrypt(payload_encrypted)
@@ -88,15 +90,20 @@ class Client:
         request.opt.observe = 0
         requester = self._client_context.request(request)
         ## https://github.com/chrysn/aiocoap/blob/1f03d4ceb969b2b443c288c312d44c3b7c3e2031/aiocoap/cli/client.py#L296
-        #observation_is_over = asyncio.get_event_loop().create_future()
-        #requester.observation.register_errback(observation_is_over.set_result)
-        #requester.observation.register_callback(lambda data, options=options: incoming_observation(options, data))
-        # exit_reason = await observation_is_over
-        # print("Observation is over: %r"%(exit_reason,), file=sys.stderr)
+        observation_is_over = asyncio.get_event_loop().create_future()
+        requester.observation.register_errback(observation_is_over.set_result)
+        requester.observation.register_callback(lambda data: on_valuechange_callback(decrypt_status(data)))#lambda data, options=options: incoming_observation(options, data))
+        
         response = await requester.response
-        yield decrypt_status(response)
-        async for response in requester.observation:
-            yield decrypt_status(response)
+        data = decrypt_status(response)
+        on_valuechange_callback(data)
+
+        exit_reason = await observation_is_over
+        logger.error("Observation is over: %r"%(exit_reason,))#, file=sys.stderr)
+        ####
+        
+        #async for response in requester.observation:
+        #    yield decrypt_status(response)
 
     async def set_control_value(self, key, value, retry_count=5, resync=True) -> None:
         return await self.set_control_values(
